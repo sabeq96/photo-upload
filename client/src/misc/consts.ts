7cb30a3a069f35572f4688291b4e29667c908562
Dawid Szafrański <dawid.szafranski@relativity.com>
2025-08-11 19:52:49 +0200

    clean

--- client/src/misc/consts.ts
+++
@@ -1,8 +0,0 @@
-export const APP_SETTINGS = {
-  apiUrl: "/api",
-  pocketbaseUrl: "/db",
-} as const;
-
-export const API_PATHS = {
-  hello: APP_SETTINGS.apiUrl + "/",
-};
--- client/src/routes/AppRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { usePocketbase } from "../hooks/usePocketbase";
-
-export function AppRoute() {
-  const { pb, login } = usePocketbase();
-
-  return <div>ok</div>;
-}
