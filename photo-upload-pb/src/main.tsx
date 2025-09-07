2c2b85e2a8359b7f581c574f66cee8fb18686ef3
Dawid Szafrański <dawid.szafranski@relativity.com>
2025-09-07 09:42:21 +0200

    add router

--- photo-upload-pb/src/main.tsx
+++ photo-upload-pb/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import { HomePage } from './pages';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
